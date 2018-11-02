a4b8fcb629a739aea09437c5286da82fb93cabf8
github username <github email address>
2018-11-02 15:38:58 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,17 +1,62 @@
 class StudentsController < ApplicationController
-  
+
+##Note::
+  #CRUD must always be in this order
+  #Everytime you create a method makesure its tested
+
+
   def index
+    #Model ==>
+    #Response/view ==> renders :---
+    # Code ==> Always gives all instances of the class
     @students = Student.all
   end
 
-  def show
-    @student = Student.find(params[:id])
-  end
-
   def new
+    #
+    #Response/view ==> renders :---
+    #code
+    # render :new
   end
 
   def create
+    #Response/view ==> resnders :---
+    #code
+    @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
+    redirect_to "/students/#{@student.id}"
   end
 
+  def show
+    #Always gives one instance of the class using its ID
+    #Response/view ==> renders :---
+    @student = Student.find(params[:id])
+  end
+
+  # def edit
+  #   #
+  #   #Response/view ==> renders :---
+  #   #code
+  # end
+  #
+  # def update
+  #   #
+  #   #Response/view ==> renders :---
+  #   #code
+  # end
+  #
+  # def destroy
+  #   #
+  #   #Response/view ==> renders :---
+  #   #code
+  # end
+
+
 end
+
+# index
+# new
+# create
+# show
+# edit
+# update
+# destory
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,27 @@
 Rails.application.routes.draw do
-  resources :students, only: [:index, :new, :create]
-  get '/student/:id', to: 'students#show', as: 'student'
+
+  ## Note::
+  # new ==> "get" from User
+  # create ==> "post" using a 'Form' to User
+  # index ==> "get" from User using URL
+  # show ==> "get" from User using URL
+  # edit ==> "get" from User using URL
+  # update ==> "patch" using a 'Form' to User
+  # destroy ==> "delete" using a 'Form' to User
+
+  # When run 'rake routes' we will see all the routes we created
+
+  #resources controller 'name', only: [:controller methods, :controller methods, :controller methods]
+  resources :students, only: [:index, :new, :create, :show]
+  # index -> get '/students', to: "students#index"
+  # get '/students/new', to: "students#new"
+  # create -> post '/students', to: "students#create"
+
+
+  #get/url ==> to: controller#method as: "path"(nickname)
+  # get '/student/:id', to: 'students#show', as: 'student'
+
+  # Does 'resources' and 'get' do thesame thing?
+    #==>
+
 end
